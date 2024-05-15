2748b0c77261885ea475d432e135fccd8e3fe66f
Ian <ian_frye@outlook.com>
2024-05-15 12:59:21 -0400

    finsihed up

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -62,7 +62,7 @@ export default function Home() {
         addPhoto={addPhoto}
         photo={photo}
       />
-      {!previewUrl ? (
+      {!previewUrl && data ? (
         <DescribeImage {...data!} />
       ) : (
         <DataComponent
